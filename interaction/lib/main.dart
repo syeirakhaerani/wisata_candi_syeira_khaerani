b4d369d54240c1028abeb7fa4c7267cb3576e1ce
syeira <syeirakhaerani@gmail.com>
2024-11-25 11:47:16 +0700

    up

---
+++ interaction/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String actionLabel = 'Belum Ada Aksi';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Interaction'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  actionLabel = 'Pengguna melakukan Tap';
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  actionLabel = 'Pengguna melakukan Double Tap';
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  actionLabel = 'Pengguna melakukan Long Press';
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Aksi',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(actionLabel),
+          ],
+        ),
+      ),
+    );
+  }
+}
